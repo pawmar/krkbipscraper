10b5fa545c1685a4da7029b14e33b5a45edacff3
Pawel Markowski <pawmar@gmail.com>
2015-06-30 22:26:26 +0200

    Added limit cli parameter

--- krkbipscraper/spiders/krkbipscraper.py
+++ krkbipscraper/spiders/krkbipscraper.py
@@ -9,6 +9,9 @@ import scrapy
 from ..items import BipItem
 
 
+HARD_LIMIT = 200
+
+
 class BipSpider(scrapy.Spider):
 
     """Spider for Krakow's BIP."""
@@ -20,21 +23,24 @@ class BipSpider(scrapy.Spider):
 
         Keyword arguments (taken from scrapy command line):
         limit -- up to how many items to scrap
-        to_date -- down to which date scrap the items
+        to_date -- down to which date (format YYYY-MM-DD) scrap
+                   the items
 
         If limit and to_date is set, limit has priority.
         """
         self.start_urls = [
             'http://bip.krakow.pl/index.php?bip_id=1&sw_id=-1&tab_id=2']
-        self.limit = limit
+        if limit is None:
+            self.limit = HARD_LIMIT
+        else:
+            self.limit = int(limit)
         self.to_date = to_date
 
     def parse(self, response):
         """Main scrapy parse function."""
-        limit = 5
         urls = response.css('.kom_row a.float_lewy::attr("href")').extract()
         for idx, url in enumerate(urls):
-            if idx < limit:
+            if idx < self.limit:
                 yield scrapy.Request(response.urljoin(url), self.parse_item)
 
     def parse_item(self, response):
